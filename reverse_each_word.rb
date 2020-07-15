926d4fa31fd4c2d7826af159afdbb7c5a77ce5f2
Timothy Lalli <trlalli92@gmail.com>
2020-07-15 01:58:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,12 @@ def reverse_each_word(words)
 end
 
 
-def reverse_each_word(words)
-  array = words.split(" ").collect do |word|
-    word.reverse 
-  end
-  return array.join(" ")
-end
+# def reverse_each_word(words)
+#   array = words.split(" ").collect do |word|
+#     word.reverse 
+#   end
+#   return array.join(" ")
+# end
 
 def reverse_each_word(words)
   return words.split(' ').collect { |w| w.reverse}.join(' ')
